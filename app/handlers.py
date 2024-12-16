1c6ae9b8edc85a7627f93fe99abb7fd4315fbb74
sora <108695017+Komeki@users.noreply.github.com>
2024-12-16 22:12:48 +0400

    Update handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -2,23 +2,31 @@ from aiogram import F, Router
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 
+import sqlite3
 import app.keyboards as kb
  
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Здравствуйте! Представьтесь, пожалуйста:', reply_markup=kb.student_curator)
+    await message.answer('Здравствуйте! Представьтесь, пожалуйста:', reply_markup=kb.student_or_curator)
+    await message.delete()
 
-@router.message(F.text == ('Я Студент'))
-async def cmd_student(message: Message):
-    await message.reply('Здорово! Давай заполним твой профиль.')
+@router.callback_query(F.data == ('its_a_student'))
+async def cmd_student(callback: CallbackQuery):
+    await callback.message.delete()
+    await callback.message.answer('Отлично, студент, давайте заполним Ваш профиль!')
+    await callback.message.answer('Введите свое ФИО: ')
 
-@router.message(F.text == ('Я Куратор'))
-async def cmd_curator(message: Message):
-    await message.reply('Отлично! Вы хотите отправить запрос на получение прав куратора?', reply_markup=kb.ifcurator)
+    conn = sqlite3.connect('student_bd.sql')
+    cur = conn.cursor()
 
-@router.callback_query(F.data == 'cur')
-async def cur(callback: CallbackQuery):
-    await callback.answer('Запрос отправлен')
-    await callback.message.answer('Ожидайте уведомления')
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@router.callback_query(F.data == 'its_a_curator')
+async def z(callback: CallbackQuery):
+    await callback.message.answer('Введите SECRET-PHRASE: ')
+    await callback.message.delete
