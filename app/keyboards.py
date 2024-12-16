d03c4f7c1f72451cc1c67a4debbb02bbe3a5b787
sora <108695017+Komeki@users.noreply.github.com>
2024-12-17 02:33:38 +0400

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -9,3 +9,30 @@ student_or_curator = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton
 # keyboard = [[кнопка] ряды ] клавиатура
 are_you_curator = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Да'), KeyboardButton(text='Нет')]], 
                                      resize_keyboard=True)
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Мой профиль")],
+        [KeyboardButton(text="Список мероприятий")],
+        [KeyboardButton(text="Мои отзывы")]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+back_button = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Назад")]
+    ],
+    resize_keyboard=True
+)
+
+curator_panel = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Мой профиль")],
+        [KeyboardButton(text="Добавить мероприятие"), KeyboardButton(text="Удалить мероприятие")],
+        [KeyboardButton(text="Актуальные мероприятия")],
+        [KeyboardButton(text="Статистика куратора")]
+    ],
+    resize_keyboard=True
+)
